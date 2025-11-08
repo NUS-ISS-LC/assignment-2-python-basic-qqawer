df921d3b7e818303d6cee3ccf9eb8a7bb61c594e
qqawer <115703733+qqawer@users.noreply.github.com>
2025-11-08 08:28:32 +0000

    Completed Assignment2 --WANGZHILI

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
 def find(s, n):
 # write your implementation here
-    return None
+     for i in range(len(s)):
+        for j in range(i+1, len(s)):
+            if s[i]+s[j]==n:
+                return [i,j]
+            
+    
